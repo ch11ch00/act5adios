b2875f722160fbfe3c836dadd9875d2e3c901251
davidgm05 <dapnestic05@gmail.com>
2023-05-10 13:13:44 +0200

    subida

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿﻿Console.WriteLine("Hola, escribe tu nombre");
+var nombre = Console.ReadLine();
+Console.Write("Adiós ");
+for (int i = (nombre.Length-1); i >= 0; i--)
+{
+    Console.Write(nombre[i]);
+}
